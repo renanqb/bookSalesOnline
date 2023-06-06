44af782823bd0edb37f320c3b7c1764345fac126
BrunoRiguetto <bruno.riguetto@gmail.com>
2023-06-06 15:56:38 -0300

    feat: implementing SubjectEntity

---
+++ src/main/java/com/renan/booksalesonline/adapters/repositories/entities/SubjectEntity.java
@@ -0,0 +1,52 @@
+package com.renan.booksalesonline.adapters.repositories.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "subject")
+public class SubjectEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    public SubjectEntity() {
+    }
+
+    public SubjectEntity(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

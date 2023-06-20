36eec406c9a6fc9164bba75b2ddd2af389d1c97f
BrunoRiguetto <bruno.riguetto@gmail.com>
2023-06-20 08:45:05 -0300

    feat: Create the SubjectDto class to represent the API output.

---
+++ src/main/java/com/renan/booksalesonline/adapters/controllers/v1/model/SubjectDto.java
@@ -0,0 +1,20 @@
+package com.renan.booksalesonline.adapters.controllers.v1.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.renan.booksalesonline.adapters.controllers.v1.commom.BaseDto;
+
+public class SubjectDto extends BaseDto {
+
+    @JsonProperty("description") private String description;
+
+    SubjectDto() { super(""); }
+
+    public SubjectDto(String name, String description) {
+        super(name);
+        this.description = description;
+    }
+
+    public String getGentilic() {
+        return description;
+    }
+}
